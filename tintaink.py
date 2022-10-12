58c42d52ca40a63165871efb08bb26df5013db2a
vitorpestana94 <115668142+vitorpestana94@users.noreply.github.com>
2022-10-12 19:02:08 -0300

    Update tintaink.py

--- tintaink.py
+++ tintaink.py
@@ -5,7 +5,7 @@ quanto metros a tinta cobra e quanto cada galão e lata contem. Este programa é
 
 """ 
 """
-#This program calculate how many cans and gallons will be necessary to paint an area provided by the input and how much it will cost. There are 3 paiting scenarios. 
+This program calculate how many cans and gallons will be necessary to paint an area provided by the input and how much it will cost. There are 3 paiting scenarios. 
 First scenery, buying only cans; second scenery, buying only gallons; third scenery, buying both cans and gallons. The program considerates the ink cover capacity and
 how many liters are avaliable to paint in the gallon and in the can. This program is a exercice from the page: https://wiki.python.org.br/EstruturaSequencial
 """
