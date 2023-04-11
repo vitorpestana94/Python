81b7fe5cc6e266456c6d47f630bf3f288580acf4
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-11 10:50:02 -0300

    Create 4.py

---
+++ Exercícios Python Brasil/Estrutura Sequencial/1-9/4/4.py
@@ -0,0 +1,4 @@
+def media(n1, n2, n3, n4):
+  return print("A média das suas notas é: {:.2f}".format((n1+n2+n3+n4)/4))
+
+media(float(input("Digite a primeira nota\n")), float(input("Digite a segunda nota\n")),float(input("Digite a terceira nota\n")), float(input("Digite a quarta nota\n")))
