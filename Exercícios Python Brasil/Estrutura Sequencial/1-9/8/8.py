45e0dcc172fee9b564b17f058e8d6a6cc43ae615
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-11 16:05:49 -0300

    Create 8.py

---
+++ Exercícios Python Brasil/Estrutura Sequencial/1-9/8/8.py
@@ -0,0 +1,3 @@
+def sporh(h, nh):
+  return print("O seu salário total ao final do mês é {}".format((h*nh)))
+sporh(float(input("Digite quanto ganhas por hora\n")),float(input("Digite quanto horas trabalhaste neste mês\n")))
