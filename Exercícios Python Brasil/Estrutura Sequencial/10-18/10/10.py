a6250c25a77d1dc2d63a93924d674b298f190b24
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-12 10:00:14 -0300

    Create 10.py

---
+++ Exercícios Python Brasil/Estrutura Sequencial/10-18/10/10.py
@@ -0,0 +1,3 @@
+def CparaF(c):
+  return print("A temperatura {} em celsius equivale à {} em fahrenheit.\n".format(c, 1.8*c + 32))
+CparaF(float(input("Digite a temperatura em celsius para ser convertida para fahrenheit.\n")))
