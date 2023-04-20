252b28c754dffc8fb5e885a67f3de86dc644ff82
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-04-20 14:52:40 -0300

    Update faturament_mensal.py

--- Entrevistas de emprego/Target Sistemas/Questão 4/Solução/faturament_mensal.py
+++ Entrevistas de emprego/Target Sistemas/Questão 4/Solução/faturament_mensal.py
@@ -11,3 +11,5 @@ def faturamento_mensal(dic):
   percentual_outros = dic["Outros"]/total*100
   
   return 'O percentual de representação que cada estado foi:\nSP: {:.1f}\nRJ: {:.1f}\nMG: {:.1f}\nES: {:.1f}\nOutros: {:.1f}'.format(percentual_SP, percentual_RJ, percentual_MG,  percentual_ES, percentual_outros)
+
+print(faturamento_mensal({'SP': 67836.43,'RJ':36678.66, 'MG':29229.88, 'ES': 27165.48, 'Outros': 19849.53}))
