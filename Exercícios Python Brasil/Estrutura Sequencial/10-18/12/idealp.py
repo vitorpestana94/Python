74eccf932ec39e8a1a97a6dfa0bdb12e9ca6b7d8
Vitor Luiz Pestana Pereira Menezes <vitorpestanatr@gmail.com>
2023-05-16 18:06:55 -0300

    Create idealp.py

---
+++ Exercícios Python Brasil/Estrutura Sequencial/10-18/12/idealp.py
@@ -0,0 +1,3 @@
+def idealp(a):
+  return print("O seu peso ideal, considerando a sua altura ({}) é {:.1f}.\n".format(a, (72.7*a) - 58))
+idealp(float(input("Digite o sua altura para calcular seu peso ideal.\n")))
